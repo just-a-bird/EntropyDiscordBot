fb7195e6d4ef5622f91781abde96d6e7fc39ac93
BASESTAR\John <burdettj@etsu.edu>
2017-09-11 20:04:36 -0400

    Added comments to some code

--- Bot.py
+++ Bot.py
@@ -30,34 +30,47 @@ async def roll(dice : str):
     subtract = False
     addative = 0
     sign = ' + '
+    ##check for valid user syntax
     try:
         ##try to map rolls to ints by splitting along d -- only works if no modifier is specified
         try:
+            ##rolls = number of dice, limit == number of faces on the dice
             rolls, limit = map(int, dice.split('d'))
             addative=0
         ##handle the modifier
         except Exception:
+            ##split using the 'd' character
+            ##[0] will be number of dice, [1] 'd', [2] everything else
             split=re.split('d',dice)
             rolls=int(split[0])
+            ##split everything after the d using + or -
             modsplit = re.split("([+-])", split[1])
+            ##limit == number of face on the dice, comes before the + or -
             limit = modsplit[0]
+            ##check if modifier is meant to subtract
             if modsplit[1] == '-':
                 subtract = True
+            ##get the actual modifier value
             modifier = modsplit[2]
             try:
+                ##try to convert modifier string to an int
                 addative=int(modifier)
             except Exception:
+                ##user-specified modifier wasn't an int
                 await bot.say('modifier must be an integer')
-  
+    ##invalid user syntax
     except Exception:
         await bot.say('The correct format is #d# + or - # (no spaces)')
         return
+        
+    ##handle a negative modifier
     if subtract:
         sign = ' - '
         diceSum = 0-addative
     else:
         diceSum=addative
         
+    ##formatting the result string and adding rolls to the total
     result = '('
     for r in range(rolls):
         die = random.randint(1,int(limit))
@@ -65,7 +78,6 @@ async def roll(dice : str):
         result += str(die)
         if r != rolls-1:
             result += ' + '
-    
     result += ') ' + sign + str(addative) + ' = ' + str(diceSum)
     await bot.say(result)
 
